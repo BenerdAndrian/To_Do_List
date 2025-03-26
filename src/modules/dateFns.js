d349652bc9243af61d6638beb3308a8175ad9c24
BenerdAndrian <ben527466@gmail.com>
2025-03-26 05:03:04 +0000

    installing date-fns package with npm, create new file dateFns.js inside module directory which will process duedate info, inside importing today,tomorrow,this week check from date-fns package

---
+++ src/modules/dateFns.js
@@ -0,0 +1,6 @@
+import { isTomorrow, isToday, isThisWeek } from "date-fns";
+const today = new Date();
+const todayList = [];
+const tomorrowList = [];
+const thisWeekList = [];
+const upComingList = [];
