dff9541d8fac6a094e51c6334faa1d5a88f68182
BenerdAndrian <ben527466@gmail.com>
2025-03-27 09:00:05 +0000

    .

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,6 @@ module.exports = merge(common, {
   mode: "development",
   devtool: "eval-source-map",
   devServer: {
-    watchFiles: ["./src/template.html"],
+    watchFiles: ["./src/index.html"],
   },
 });
